9c41954c5b402223abf9fcc5cf51d795f21d153d
Natnael-Gedlu <nategd800@gmail.com>
2024-05-29 21:57:10 +0300

    added docstrings

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,22 @@
+#!/usr/bin/python3
+"""
+Defines the User model for a Flask application
+"""
 from backend import db
 from flask_login import UserMixin
 
 
 class User(db.Model, UserMixin):
+    """
+    Represents a user with id, username, email, and password.
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
+        """
+        Returns a string representation of the user.
+        """
         return f'<User {self.username}>'
