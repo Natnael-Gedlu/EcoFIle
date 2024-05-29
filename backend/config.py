67c7ecf1574bdd5cac5860d8479adc7494e7185e
Natnael-Gedlu <nategd800@gmail.com>
2024-05-29 22:06:19 +0300

    Config for Flask app

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,9 @@ import os
 
 
 class Config:
+    """
+    Configuration class for the Flask application.
+    """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
